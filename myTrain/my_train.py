226a73a9ad92184cb3414a2f5cc62db2bbfeae18
ssano <kssho3364@gmail.com>
2022-06-19 00:15:05 +0900

    20220619_KSH
    기본

---
+++ myTrain/my_train.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+from tflite_model_maker import model_spec
+from tflite_model_maker import image_classifier
+from tflite_model_maker.image_classifier import DataLoader
+
+# image_path = tf.keras.utils.get_file(
+#       'flower_photos',
+#       'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
+#       untar=True)
+image_path = 'C:/Users/sano/.keras/datasets/flower_photos'
+
+data = DataLoader.from_folder(image_path)
+train_data, test_data = data.split(0.9)
+
+model = image_classifier.create(train_data)
+
+loss, accuracy = model.evaluate(test_data)
+
+model.export(export_dir='.', with_metadata=False)
